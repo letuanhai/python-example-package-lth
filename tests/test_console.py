94f67c5f36b1171d6d2dafdd1cd6c2f487f305ef
letuanhai <letuanhai@live.com>
2022-03-15 22:47:08 +0700

    Add test cases for main

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,3 +22,20 @@ def mock_requests_get(mocker):
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_print_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_use_en_wikipedia_org(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+
+def test_main_fail_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Failed")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
