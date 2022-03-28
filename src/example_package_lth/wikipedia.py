96afe1cb55f797a1c14031ae330a63b86f995dc0
letuanhai <letuanhai@live.com>
2022-03-28 22:34:58 +0700

    Add error handling for wikipedia client

--- src/example_package_lth/wikipedia.py
+++ src/example_package_lth/wikipedia.py
@@ -1,9 +1,17 @@
+from email import message
+from pydoc import cli
+from urllib.request import Request
 import requests
+import click
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
