241299992b28c2eff87f94048bac5e6db2094675
letuanhai <letuanhai@live.com>
2022-03-29 11:53:56 +0700

    Add language option for main

--- src/example_package_lth/console.py
+++ src/example_package_lth/console.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import textwrap
 
 import click
@@ -6,10 +7,18 @@ from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
