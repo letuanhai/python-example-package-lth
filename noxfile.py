587f955c1970b58ab41344d4a9ae72039c47b2d8
letuanhai <letuanhai@live.com>
2022-04-09 10:23:34 +0700

    Add feature: check imports with flake8-import-order

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ def tests(session):
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
