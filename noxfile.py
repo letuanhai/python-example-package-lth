6ec52eb246efe978e3401209915695b90ea7ae54
letuanhai <letuanhai@live.com>
2022-04-09 10:19:26 +0700

    Add feature: formatting with black

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 import nox
 
+nox.options.sessions = "lint", "tests"
+
 locations = "src", "tests", "noxfile.py"
 
 
@@ -13,5 +15,12 @@ def tests(session):
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
+
+
+@nox.session(python="3.9")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
