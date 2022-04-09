4ff735e78ab027948a935d2e4fe94515ea110f38
letuanhai <letuanhai@live.com>
2022-04-09 10:33:09 +0700

    Add feature: additional bug checking with bugbear, bandit and safety

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 import nox
+import tempfile
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "tests", "safety"
 
 locations = "src", "tests", "noxfile.py"
 
@@ -15,7 +16,13 @@ def tests(session):
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install(
+        "flake8",
+        "flake8-black",
+        "flake8-import-order",
+        "flake8-bugbear",
+        "flake8-bandit",
+    )
     session.run("flake8", *args)
 
 
@@ -24,3 +31,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
